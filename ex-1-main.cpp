f49c8dcce021f59863c05e33180b830d00c17e06
RYUSEI BESSHO <ht18a092@oecu.jp>
2020-11-24 12:14:42 +0900

    bool is_primeの訂正

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -14,10 +14,10 @@
 bool is_prime(int num){
     if ( num ==1 ) return false;
     for(int i=2; i<=num-1; i++){
+        if(n%i==0)
         return false;
     } 
         return true;
-
 }
 int nth_prime(unsigned int a, unsigned int d, unsigned int n){
      int num = a;
